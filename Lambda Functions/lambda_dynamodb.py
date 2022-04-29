579e829f2a1a2181317c6f5352dc45669e01dcf6
greeshma-tatineni <91391787+greeshma-tatineni@users.noreply.github.com>
2022-04-29 16:40:50 -0400

    Add files via upload

---
+++ Lambda Functions/lambda_dynamodb.py
@@ -0,0 +1,29 @@
+import json
+import boto3
+from decimal import Decimal
+#from boto3.dynamodb2.table import Table
+#import boto3.dynamodb
+
+def lambda_handler(event, context):
+    s3 = boto3.client("s3")
+    dynamodb = boto3.resource('dynamodb')
+    if event:
+        print("started")
+        file_obj = event["Records"][0]
+        bucket_name = str(file_obj["s3"]["bucket"]["name"])
+        print(bucket_name)
+        file_name = str(file_obj["s3"]["object"]["key"])
+        print(file_name)
+        json_object = s3.get_object(Bucket=bucket_name, Key=file_name)
+        jsonFileReader = json_object['Body'].read()
+        jsonDict = json.loads(jsonFileReader, parse_float=Decimal)
+        #print(json.loads(jsonFileReader)
+        #print(dynamodb.list_tables()['TableNames'])
+        table = dynamodb.Table('sentiment')
+        table.put_item(Item=jsonDict)
+        print("ingested into table sentiment")
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
