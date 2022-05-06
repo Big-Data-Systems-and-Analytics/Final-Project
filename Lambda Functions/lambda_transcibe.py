04e73aa776bcf1b883583f919e758b4bc83ebbbb
greeshma-tatineni <91391787+greeshma-tatineni@users.noreply.github.com>
2022-05-05 22:52:35 -0400

    Update lambda_transcibe.py

--- Lambda Functions/lambda_transcibe.py
+++ Lambda Functions/lambda_transcibe.py
@@ -6,10 +6,13 @@ from urllib.request import urlopen
 
 def lambda_handler(event, context):
     # TODO implement
+    print("even",str(event))
     print("started.....")
     transcribe = boto3.client("transcribe")
     s3 = boto3.client("s3")
     if event:
+        
+        #fetching the audio file from s3 bucket
         file_obj = event["Records"][0]
         bucket_name = str(file_obj["s3"]["bucket"]["name"])
         print(bucket_name)
@@ -18,11 +21,12 @@ def lambda_handler(event, context):
         s3_uri = create_uri(bucket_name, file_name)
         file_type = file_name.split(".")[1]
         job_name = context.aws_request_id
-
+        
+        #invoking Transcribe Lambda
         transcribe.start_transcription_job(
             TranscriptionJobName=job_name,
             Media={"MediaFileUri": s3_uri},
-            MediaFormat=file_type,
+            MediaFormat="wav",
             LanguageCode="en-US"
             # Settings={
             #     # 'VocabularyName': 'string',
@@ -37,10 +41,12 @@ def lambda_handler(event, context):
                 break
             print("its in progress")
             time.sleep(10)
-            
+        
+        #getting generated transcripts    
         load_url = urlopen(status['TranscriptionJob']['Transcript']['TranscriptFileUri'])
         load_json = json.dumps(json.load(load_url))
         
+        #Writing json file to s3 bucket
         s3.put_object(Bucket = bucket_name, Key = "transcripts/{}.json".format(file_name.split("/")[1]+"_"+file_name.split("/")[2].split(".")[0]), Body=load_json)
         
     return {
